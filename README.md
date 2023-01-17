03e942f1323ea2d33e557d5f51ddb7d8b43d0171
ittp <61617660+ittp@users.noreply.github.com>
2023-01-17 15:34:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-hbgbcj-18h4rg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-hbgbcj-18h4rg)
---
+++ pages/index.tsx
@@ -0,0 +1,90 @@
+import { log } from 'console';
+import { randomUUID } from 'crypto';
+import { useCallback, useEffect, useState } from 'react';
+import Button from '../components/Button';
+import ClickCount from '../components/ClickCount';
+import styles from '../styles/home.module.css';
+
+function throwError() {
+  console.log(
+    // The function body() is not defined
+    // @ts-ignore
+    document.body()
+  );
+}
+
+function onSuccess(message: string) {
+  console.log('success', message);
+}
+
+const DateTime = (dt: any) => {
+  // let d = new Date();
+
+  return dt.getMinutes() + ':' + dt.getMilliseconds();
+};
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  const increment = useCallback(() => {
+    setCount((v) => v + 1);
+
+    // let time = DateTime();
+    // setCount((v) => {
+    //   console.log(v);
+    //   return v + 1;
+
+    //   // return DateTime(dt).toString();
+    // });
+  }, [setCount]);
+
+  useEffect(() => {
+    const r = setInterval(() => {
+      increment();
+    }, 1000);
+
+    return () => {
+      clearInterval(r);
+    };
+  }, [increment]);
+
+  return (
+    <main className={styles.main}>
+      <h1>Fast Refresh Demo</h1>
+      <p>
+        Fast Refresh is a Next.js feature that gives you instantaneous feedback
+        on edits made to your React components, without ever losing component
+        state.
+      </p>
+      <hr className={styles.hr} />
+      <div>
+        <p>
+          Auto incrementing value. The counter won't reset after edits or if
+          there are errors.
+        </p>
+        <p>Current value: {count}</p>
+      </div>
+      <hr className={styles.hr} />
+      <div>
+        <p>Component with state.</p>
+        <ClickCount />
+      </div>
+      <hr className={styles.hr} />
+      <div>
+        <p>
+          The button below will throw 2 errors. You'll see the error overlay to
+          let you know about the errors but it won't break the page or reset
+          your state.
+        </p>
+        <Button
+          onClick={(e) => {
+            setTimeout(() => document.parentNode, 0);
+            throwError();
+          }}
+        >
+          Throw an Error
+        </Button>
+      </div>
+      <hr className={styles.hr} />
+    </main>
+  );
+}
